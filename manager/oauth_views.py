fadacbf08d6b1931007a5ffdc8edc421a79f3d02
Yauheni <Karasevgen1205@yandex.ru>
2021-01-11 20:44:10 +0300

    add oauth 2 with github part 3

--- manager/oauth_views.py
+++ manager/oauth_views.py
@@ -5,12 +5,14 @@ from book_shop.settings import GIT_CLIENT_ID, GIT_CLIENT_SECRET
 
 def complete_github_view(request):
     url = f"https://github.com/login/oauth/authorize?client_id={GIT_CLIENT_ID}"
-    return render(request, "brazzers.html", {"url": url})
+    return render(request, "complete.html", {"url": url})
 
 
 def complete_github_callback(request):
     code = request.GET.get("code")
-    url = f"https://github.com/login/oauth/access_token?client_id={GIT_CLIENT_ID}&client_secret={GIT_CLIENT_SECRET}&code={code}"
+    url = f"https://github.com/login/oauth/access_token?client_id={GIT_CLIENT_ID}&" \
+          f"client_secret={GIT_CLIENT_SECRET}&" \
+          f"code={code}"
     response = post(url, headers={'Accept': 'application/json'})
     access_token = response.json()['access_token']
     url = "https://api.github.com/user"
@@ -19,4 +21,4 @@ def complete_github_callback(request):
     url = f"https://api.github.com/users/{login}/repos"
     response = get(url)
     repos = [i['name'] for i in response.json()]
-    return render(request, "brazzers.html", {'data': repos})
+    return render(request, "complete.html", {'data': repos})
