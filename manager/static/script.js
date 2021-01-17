5fe3d411170fa91b1b251266deca22a78450fc3a
Yauheni <Karasevgen1205@yandex.ru>
2021-01-17 20:51:06 +0300

    add load satic and add javascript 4

--- manager/static/script.js
+++ manager/static/script.js
@@ -12,6 +12,18 @@ $('document').ready(function () {
                 $("#" + id).html('Likes: ' + data['likes']);
             }
         })
-
     });
+
+    $('.delete-comment').on('click', function () {
+        let id=$(this).attr('id');
+        $.ajax({
+            url: "/shop/delete_comment_ajax",
+            data: {"comment_id": id.split("-")[2]},
+            method: "GET",
+            success: function (data) {
+                $(`#${id}`).remove()
+            }
+        })
+    })
+
 });
