f3e6f5960d6b89e44f6e581364347560d44db3c1
Yauheni <Karasevgen1205@yandex.ru>
2021-01-17 20:57:16 +0300

    add load satic and add javascript 5

--- manager/static/script.js
+++ manager/static/script.js
@@ -1,15 +1,12 @@
 $('document').ready(function () {
     $('.like-comment').on('click', function () {
-
         let id=$(this).attr('id');
-        console.log('hello click', id);
-
         $.ajax({
             url:"/shop/add_like2comment_ajax",
             data: {"comment_id": id.split('-')[1]},
             method: "GET",
             success: function (data) {
-                $("#" + id).html('Likes: ' + data['likes']);
+                $(`#${id}`).html(`Likes: ${data['likes']}`);
             }
         })
     });
@@ -25,5 +22,4 @@ $('document').ready(function () {
             }
         })
     })
-
 });
--- manager/views_ajax.py
+++ manager/views_ajax.py
@@ -11,3 +11,13 @@ def add_like2comment(request):
         count_likes = comment.users_like.count()
         return JsonResponse({"likes": count_likes}, status=status.HTTP_201_CREATED)
     return JsonResponse({"error": "User is not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+def delete_comment(request):
+    if request.user.is_authenticated:
+        comment = Comment.objects.get(id=request.GET.get("comment_id"))
+        if request.user == comment.author:
+            comment.delete()
+            return JsonResponse({}, status=status.HTTP_204_NO_CONTENT)
+        return JsonResponse({}, status=status.HTTP_403_FORBIDDEN)
+    return JsonResponse({}, status=status.HTTP_401_UNAUTHORIZED)
