a8c8a3336cdf78fc969d23eea161ec553d74f359
Yauheni <Karasevgen1205@yandex.ru>
2021-01-17 20:06:26 +0300

    add load satic and add javascript 2

--- book_shop/settings.py
+++ book_shop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manager',
+    'rest_framework',
     # 'users',
     # 'oauth2_provider',
     'debug_toolbar',
--- manager/views_ajax.py
+++ manager/views_ajax.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
-from manager.models import LikeComment
+from django.http import HttpResponse, JsonResponse
+from rest_framework import status
+from manager.models import LikeComment, Comment
 
 
 def add_like2comment(request):
     if request.user.is_authenticated:
-        LikeComment.objects.create(user=request.user, comment_id=request.GET.get('comment_id'))
-    return HttpResponse("OK")
+        comment_id = request.GET.get('comment_id')
+        LikeComment.objects.create(user=request.user, comment_id=comment_id)
+        comment = Comment.objects.get(id=comment_id)
+        count_likes = comment.users_like.count()
+        return JsonResponse({"likes": count_likes}, status=status.HTTP_201_CREATED)
+    return JsonResponse({"error": "User is not authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
