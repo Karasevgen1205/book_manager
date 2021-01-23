31b86957f7e2e621fac86971a6cf239012d3f308
Yauheni <Karasevgen1205@yandex.ru>
2021-01-23 10:10:44 +0300

    add new app - articles 4

--- articles/models.py
+++ articles/models.py
@@ -18,10 +18,11 @@ class Article(models.Model):
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
 
+
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     author_name = models.CharField('Имя автора', max_length=50)
-    comment_text = models.TextField('Текст комментария', max_length= 200)
+    comment_text = models.TextField('Текст комментария', max_length=200)
 
     def __sty__(self):
         return self.author_name
@@ -32,6 +33,7 @@ class Comment(models.Model):
     class Meta:
         verbose_name = 'Комментарий'
         verbose_name_plural = 'Комментарии'
+
 
 class Comment4comment(models.Model):
     article = models.ForeignKey(Comment, on_delete=models.CASCADE)
