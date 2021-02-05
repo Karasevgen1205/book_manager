61691c1e3a3fccea9119d4b9af68ff3444358f73
Yauheni <Karasevgen1205@yandex.ru>
2021-02-05 22:51:57 +0300

    work with ajax and using REST framework for authtoken with testcase 13

--- manager/static/app.js
+++ manager/static/app.js
@@ -2,58 +2,146 @@
 
 // camalCase
 
-// variable
-var name = 'Yauheni' // Устаревшая и глобальная
-const lastName = 'Karas'
-const isProgrammer = true // boolean
-let age = 26 // number
-const _private = 'private'
-const _$private = 'private'
-const withNum5 = 5
+// // variable
+// var name = 'Yauheni' // Устаревшая и глобальная
+// const lastName = 'Karas'
+// const isProgrammer = true // boolean
+// let age = 26 // number
+// const _private = 'private'
+// const _$private = 'private'
+// const withNum5 = 5
 
-name = 'Yauhen'
-age = 29
+// name = 'Yauhen'
+// age = 29
 
-console.log(name)
+// console.log(name)
 
-// 2 Мутирование
+// // 2 Мутирование
 
-console.log('Имя человека: ' + name + ' , а возраст человека: ' + age)
-console.log(age)
-console.log(age.toString())
+// console.log('Имя человека: ' + name + ' , а возраст человека: ' + age)
+// console.log(age)
+// console.log(age.toString())
 
-//alert('Are you sure?')
+// //alert('Are you sure?')
 
-const firstName = prompt('Entry you name') // Окно ввода по типу alert
-alert(firstName + ' ' + lastName)
+// const firstName = prompt('Entry you name') // Окно ввода по типу alert
+// alert(firstName + ' ' + lastName)
 
-// 3 Операторы
+// // 3 Операторы
 
-let currentYear = 2020
-let birthYear = 1991
+// let currentYear = 2020
+// let birthYear = 1991
 
-let fullAge = currentYear - birthYear
-console.log(fullAge)
+// let fullAge = currentYear - birthYear
+// console.log(fullAge)
 
-const a = 10
-const b = 5
-let c = 32
+// const a = 10
+// const b = 5
+// let c = 32
 
-console.log(a - b)
-console.log(a + b)
-console.log(a / b)
-console.log(a * b)
-console.log(currentYear++) // ++ добавляет при следуюещм вызове 1 (можно в начале или в конце)
-console.log(currentYear--) // -- отнимает при следуюещм вызове 1 (можно в начале или в конце)
-console.log(currentYear)
+// console.log(a - b)
+// console.log(a + b)
+// console.log(a / b)
+// console.log(a * b)
+// console.log(currentYear++) // ++ добавляет при следуюещм вызове 1 (можно в начале или в конце)
+// console.log(currentYear--) // -- отнимает при следуюещм вызове 1 (можно в начале или в конце)
+// console.log(currentYear)
 
-c = c + a
-c += a
-c -= a
-c /= a
-c *= a
+// c = c + a
+// c += a
+// c -= a
+// c /= a
+// c *= a
 
 // 4 Типы данных
 
+// const isProgrammer = true
+// const name = 'Yauheni'
+// const age = 26
+// let x
+// null
+
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x)
+// console.log(typeof null)
 
 
+// 5 Приоритет операторов
+
+// const fullAge = 29
+// const birthYear = 1991
+// const currentYear = 2021
+
+// const isFullAge = currentYear - birthYear >= fullAge // 29 >= 30 > true
+// console.log(isFullAge)
+
+// 6 Условные операторы
+
+// const courseStatus = 'pending' // 'ready', 'fail'
+
+// if (courseStatus === 'pending') {
+//     console.log('Курс находится в стадии разрабокти')
+// } else if (courseStatus === 'ready') {
+//     console.log('Курс уже готов')
+// } else {
+//     console.log('Pull Shet!')
+// }
+
+// Работа с IF с типом данных Boolean
+
+// const isReady = true
+
+// if (isReady) {
+//     console.log('Ok')
+// } else {
+//     console.log('Fuck')
+// }
+
+// Тернарное выражение (альтернатива привычному IF ELSE):
+// isReady ? console.log('Ok') : console.log('Fuck')
+
+// Особенность работы с == и ===
+
+// const num1 = 42 // number
+// const num2 = '42' // string
+
+// console.log(num1 == num2) // true
+// console.log(num1 === num2) // false
+
+// 7 Булевая логика
+
+// AND
+// true && true  // true
+// true && false // false
+// false && true // false
+// false && false // false
+
+// OR
+// true || true  // true
+// true || false // true
+// false || true // true
+// false || false // false
+
+// 8 Функции
+
+function calculateAge(year){
+    return 2021 - year
+}
+
+const myAge = calculateAge(1991)
+console.log(myAge)
+
+console.log(calculateAge(1991))
+console.log(calculateAge(1999))
+console.log(calculateAge(2000))
+
+function logInfoAbout(name, year){
+    const age = calculateAge(year)
+    console.log('Human ' + name + ' have age ' + age)
+}
+
+logInfoAbout('Yauheni', 1991)
+
+ 
