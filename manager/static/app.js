27def4c294f4d2499406586e02a7b657f9c627ef
Yauheni <Karasevgen1205@yandex.ru>
2021-02-05 17:00:19 +0300

    work with ajax and using REST framework for authtoken with testcase 11

--- manager/static/app.js
+++ manager/static/app.js
@@ -1,18 +1,31 @@
 // 1 Переменные
-// variable
-
-alert('Are you sure?')
 
 // camalCase
 
+// variable
 var name = 'Yauheni' // Устаревшая и глобальная
 const lastName = 'Karas'
 const isProgrammer = true // boolean
 let age = 26 // number
+const _private = 'private'
+const _$private = 'private'
+const withNum5 = 5
 
 name = 'Yauhen'
 age = 29
 
-
 console.log(name)
+
+// 2 Мутирование
+
+console.log('Имя человека: ' + name + ' , а возраст человека: ' + age)
+console.log(age)
+console.log(age.toString())
+
+//alert('Are you sure?')
+
+const firstName = prompt('Entry you name') // Окно ввода по типу alert
+alert(firstName + ' ' + lastName)
+
+// 3 Операторы
 
